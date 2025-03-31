76e8cd63c81c13a84e4249466d3b502595c50b33
Riguz Lee <dr@riguz.com>
2025-03-31 21:52:46 +0800

    refactor breathe effect

--- main/include/led/rgb_led.h
+++ main/include/led/rgb_led.h
@@ -10,6 +10,12 @@
 #include "led.h"
 
 namespace walle {
+struct rgb_color {
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
+};
+
 class rgb_led : public led {
 public:
   rgb_led(gpio_num_t pin);
@@ -19,7 +25,7 @@ public:
   void on_state_changed() override;
   void show(uint8_t r, uint8_t g, uint8_t b);
   void blink(uint8_t r, uint8_t g, uint8_t b, int interval_ms);
-  void breathe();
+  void breathe(int interval_ms);
   void turn_off();
 
 private:
@@ -31,6 +37,11 @@ private:
   esp_timer_handle_t strip_timer = nullptr;
   std::mutex mutex;
   std::function<void()> strip_callback = nullptr;
+  bool blink_state = false;
+  rgb_color blink_color = {};
+  uint16_t phase = 0;
+  uint16_t hue = 0;
+  uint8_t saturation = 255;
 };
 } // namespace walle
 
--- main/src/led/rgb_led.cpp
+++ main/src/led/rgb_led.cpp
@@ -1,11 +1,17 @@
 #include "led/rgb_led.h"
-#include <esp_log.h>
-
 #include "application.h"
+#include "led/fast_hsv2rgb.h"
+#include <esp_log.h>
+#include <math.h>
+#include <stdio.h>
 
 using namespace walle;
 
 #define TAG "RgbLed"
+
+const float PI = 3.14159265f;
+const float HUE_SPEED = 3.0f;      // 色相变化速度（度/循环周期）
+const float BREATHE_SPEED = 0.03f; // 呼吸速度（相位增量/循环周期）
 
 rgb_led::rgb_led(gpio_num_t pin) : pin(pin) {
   led_strip_config_t strip_config = {
@@ -59,7 +65,7 @@ void rgb_led::on_state_changed() {
   ESP_LOGI(TAG, "Received app state change: %d", state);
   switch (state) {
   case starting:
-    show(20, 0, 0);
+    breathe(20);
     break;
 
   default:
@@ -74,9 +80,35 @@ void rgb_led::show(uint8_t r, uint8_t g, uint8_t b) {
   led_strip_refresh(this->led_strip);
 }
 
-void rgb_led::blink(uint8_t r, uint8_t g, uint8_t b, int interval_ms) {}
+void rgb_led::blink(uint8_t r, uint8_t g, uint8_t b, int interval_ms) {
+  this->blink_color = rgb_color{r, g, b};
+  start_timer(interval_ms, [this]() {
+    ESP_LOGI(TAG, "blinking...%d", this->blink_state);
+    if (this->blink_state) {
+      auto color = this->blink_color;
+      led_strip_set_pixel(this->led_strip, 0, color.r, color.g, color.b);
+      led_strip_refresh(this->led_strip);
+    } else {
+      led_strip_clear(this->led_strip);
+    }
+    this->blink_state = !this->blink_state;
+  });
+}
 
-void rgb_led::breathe() {}
+void rgb_led::breathe(int interval_ms) {
+  start_timer(interval_ms, [this]() {
+    float brightness = (sin(phase) + 1) * 127.5f;
+    uint8_t r, g, b;
+    fast_hsv2rgb_32bit(hue, saturation, (uint8_t)roundf(brightness), &r, &g,
+                       &b);
+    led_strip_set_pixel(this->led_strip, 0, r, g, b);
+    led_strip_refresh(this->led_strip);
+
+    // 更新色相和呼吸相位
+    this->hue = (uint16_t)(this->hue + HUE_SPEED) % HSV_HUE_MAX;
+    this->phase = fmod(this->phase + BREATHE_SPEED, 2 * PI);
+  });
+}
 
 void rgb_led::turn_off() {}
 
@@ -88,4 +120,5 @@ void rgb_led::start_timer(int interval_ms, std::function<void()> callback) {
   esp_timer_stop(this->strip_timer);
   this->strip_callback = callback;
   esp_timer_start_periodic(this->strip_timer, interval_ms * 1000);
+  ESP_LOGI(TAG, "LED timer started");
 }
