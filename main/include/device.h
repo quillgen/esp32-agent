16334dc6786ffdfa35b404ae81783181a3250ccc
Riguz Lee <dr@riguz.com>
2025-07-03 21:29:11 +0800

    updated speaker setting

--- main/include/device.h
+++ main/include/device.h
@@ -1,12 +1,12 @@
 #ifndef __DEVICE_H_
 #define __DEVICE_H_
 
-#define I2C_SDA_PIN 4
-#define I2C_SCL_PIN 5
+#define I2C_SDA_PIN 5
+#define I2C_SCL_PIN 4
 
-#define I2S_DOUT 6
-#define I2S_BCLK 7
-#define I2S_LRC 15
+#define I2S_DOUT 7
+#define I2S_BCLK 15
+#define I2S_LRC 16
 
 namespace agent {
 class Device {
